704fc8cf42048cc22ffaf092985c797dc7899c4f
Dj <debanikroy92@gmail.com>
2024-01-08 15:37:56 +0900

    diff_bbx =

--- human_follow_rs_hand_track.py
+++ human_follow_rs_hand_track.py
@@ -21,6 +21,7 @@ class RealSenseYoloHandTracker:
         self.ox_human_bbx_2d_rr = 300
         self.img_ox = 50
         self.img_oy = 10
+        self.num_point_obs = 25
     
     def obstracle_layer(self, frame):
         Y, X, _ = frame.shape
@@ -29,9 +30,18 @@ class RealSenseYoloHandTracker:
         Iy = Y //2
 
         human_bbx_2d_AA = (Ix - self.ox_human_bbx_2d_rr, self.img_ox)
+        human_bbx_2d_BB = (Ix + self.ox_human_bbx_2d_rr, self.img_oy)
         human_bbx_2d_CC = (Ix + self.ox_human_bbx_2d_rr, Y-self.img_oy)
 
         cv2.rectangle(frame, human_bbx_2d_AA, human_bbx_2d_CC, (0, 0, 255), 2)
+
+        diff_bbx = np.abs(human_bbx_2d_AA[0] - human_bbx_2d_BB[0]) // self.num_point_obs
+        if(diff_bbx > 0):
+            for i in np.arange(human_bbx_2d_AA[0], human_bbx_2d_CC[0] + diff_bbx, diff_bbx):
+                for j in np.arange(human_bbx_2d_AA[1], human_bbx_2d_CC[1] + diff_bbx, diff_bbx):
+                    x_int = int(round(i))
+                    y_int = int(round(j))
+                    cv2.circle(frame, (x_int, y_int), radius=1, color=(0, 255, 0), thickness=-1)
 
 
 
