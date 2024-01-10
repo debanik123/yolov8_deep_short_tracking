f77fab89c017ec08b8c4789ceef3f078bc492496
Dj <debanikroy92@gmail.com>
2024-01-10 09:52:41 +0900

    dict form

--- tracker.py
+++ tracker.py
@@ -95,11 +95,14 @@ class Tracker:
     def keypoints_utils(self, frame, results):
 
         keypoints_tensor = results[0].keypoints.data.tolist()
-        
+
         yolo_bboxes, yolo_scores, yolo_classes = self.bbx_utils(results)
         self.tracks_ = self.update(frame, yolo_bboxes, yolo_scores, yolo_classes)
 
-        for kps, track in zip(keypoints_tensor, self.tracks_):
+        dict_ = {str(idx): track.track_id for idx, (kps, track) in enumerate(zip(keypoints_tensor, self.tracks_))}
+        print(dict_)
+
+        for idx, (kps, track) in enumerate(zip(keypoints_tensor, self.tracks_)):
             try:
                 right_kps = Keypoints(frame, kps[12],kps[6], kps[8])
                 left_kps = Keypoints(frame, kps[11],kps[5], kps[7])
@@ -111,10 +114,10 @@ class Tracker:
                 # print("Left angle between hip, shoulder, and elbow:", left_angle, idx)
 
                 if right_angle is not None and right_angle > 70 and right_angle < 95:
-                    self.unique_id = track.track_id
+                    self.unique_id = dict_.get(str(idx))
                     print("Start following the person with ID: ", self.unique_id)
 
-                if left_angle is not None and left_angle > 70 and left_angle < 95 and track.track_id == self.unique_id:
+                if left_angle is not None and left_angle > 70 and left_angle < 95:
                     self.isFollowing = False
                     self.unique_id = None
                     print("Stop following the person with ID: ", self.unique_id)
