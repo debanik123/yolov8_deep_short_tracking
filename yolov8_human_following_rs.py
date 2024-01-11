8c8d058826d146e2067c4c6179f7d132eb6f864b
Dj <debanikroy92@gmail.com>
2024-01-11 09:43:55 +0900

    updated class object

--- yolov8_human_following_rs.py
+++ yolov8_human_following_rs.py
@@ -3,44 +3,73 @@ import pyrealsense2 as rs
 from ultralytics import YOLO
 import numpy as np
 
-# Load the YOLOv8 model
-model = YOLO('yolov8n-pose.pt')
+import rclpy
+from rclpy.node import Node
 
-# Configure RealSense pipeline
-pipeline = rs.pipeline()
-config = rs.config()
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+class YOLOv8TrackingNode(Node):
+    def __init__(self):
+        super().__init__('yolov8_tracking_node')
+        self.model = YOLO('yolov8n-pose.pt')
+        self.check_camera_connection()
+        self.pipeline = rs.pipeline()
+        self.config = rs.config()
+        self.config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+        self.pipeline.start(self.config)
 
-# Start streaming
-pipeline.start(config)
+        
+    
+    def check_camera_connection(self):
+        ctx = rs.context()
+        devices = ctx.query_devices()
+        if not devices:
+            raise RuntimeError("No RealSense devices found. Connect a RealSense camera and try again.")
 
-try:
-    while True:
-        # Wait for a coherent pair of frames: depth and color
-        frames = pipeline.wait_for_frames()
-        color_frame = frames.get_color_frame()
+    def process_frames(self):
+        while True:
+            frames = self.pipeline.wait_for_frames()
+            color_frame = frames.get_color_frame()
 
-        if not color_frame:
-            continue
+            if not color_frame:
+                return
 
-        # Convert RealSense frame to OpenCV format
-        frame = np.asanyarray(color_frame.get_data())
+            frame = np.asanyarray(color_frame.get_data())
 
-        # Run YOLOv8 tracking on the frame, persisting tracks between frames
-        results = model.track(frame, persist=True, classes=[0], conf=0.60, iou=0.7, max_det=10)
-        keypoints_tensor = results[0].keypoints.data.tolist()
+            results = self.model.track(frame, persist=True, classes=[0], conf=0.60, iou=0.7, max_det=10)
+            keypoints_tensor = results[0].keypoints.data.tolist()
+            boxes_tensor = results[0].boxes.data.tolist()
 
-        # Visualize the results on the frame
-        annotated_frame = results[0].plot()
+            for res in boxes_tensor:
+                x1 = int(res[0])
+                y1 = int(res[1])
+                x2 = int(res[2])
+                y2 = int(res[3])
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
-        # Display the annotated frame
-        cv2.imshow("YOLOv8 Tracking", annotated_frame)
+            cv2.imshow("YOLOv8 Tracking", frame)
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
 
-        # Break the loop if 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
+            rclpy.spin_once(self, timeout_sec=0.0000001)
 
-finally:
-    # Stop streaming
-    pipeline.stop()
-    cv2.destroyAllWindows()
+    def destroy_node(self):
+        self.pipeline.stop()
+        cv2.destroyAllWindows()
+        super().destroy_node()
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    yolov8_tracking_node = YOLOv8TrackingNode()
+
+    try:
+        rclpy.spin(yolov8_tracking_node)
+    except KeyboardInterrupt:
+        pass
+
+    yolov8_tracking_node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
